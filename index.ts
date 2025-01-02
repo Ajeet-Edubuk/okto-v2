a66701a84c881ddc7c3c683cc5dfdd79de4b0ed5
ajeetram <86039147+ajeetram@users.noreply.github.com>
2025-01-02 10:01:58 +0530

    cv-on-chain-server

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/cv", cvRouter);
+app.use("/api/v1/cv", cvRouter);
 
 app.get("/", (req: Request, res: Response) => {
   return res.json({
