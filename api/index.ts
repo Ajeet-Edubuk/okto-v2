d92c6e72b4c5edf9eae5bfce8c60fdc2a9239779
ajeetram <86039147+ajeetram@users.noreply.github.com>
2025-01-02 10:06:14 +0530

    cv-on-chain-server

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { config } from "dotenv";
-import { MongoConnection } from "./database/mongo.connection";
-import cvRouter from "./routers/cv.router";
+import { MongoConnection } from "../database/mongo.connection";
+import cvRouter from "../routers/cv.router";
 import bodyParser from "body-parser";
 import cors from "cors";
 
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/api/v1/cv", cvRouter);
+app.use("/cv", cvRouter);
 
 app.get("/", (req: Request, res: Response) => {
   return res.json({
